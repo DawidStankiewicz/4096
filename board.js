dec56b0d30555825bf957bdc84af736c384bc4eb
unknown <2ft@s1bk11.egzamin.local>
2017-11-29 14:17:09 +0100

    phaser version
    - add playState
    - add board
    - add phaser.min.js

---
+++ board.js
@@ -0,0 +1,12 @@
+var Board = function() {
+    
+}
+
+Board.prototype.create = function() {
+   alert('123');
+}
+
+var Field = function(val) {
+    this.val = val;
+}
+
---
+++ play.js
@@ -0,0 +1,21 @@
+var playState = {
+
+    preload: function () {
+        var board = new Board();
+        board.create();
+    },
+
+    create: function () {
+        
+    },
+
+    update: function () {
+
+    },
+
+}
+
+
+
+
+
