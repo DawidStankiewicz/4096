4e0554217704f095c7074ff72e8ac4043ce8812a
Dawid Stankiewicz <theszczypiorek@gmail.com>
2017-12-17 18:12:57 +0100

    mvp version
    - code needs optimization
    - added moves (up, down, left and right) and detection in play state
    - minified files
    - new assets
    
    methods in board.js:
    - load
    - create
    - create fields
    - create field
    - get fields by col and row
    - find random empty field
    - move up, move down, move right, move left - these need to be refactored
    - move to - create animation for field
    - move field to - by column and row
    - merge fields
    - is field empty - return true if there is no field in the indicated place

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(600, 600, Phaser.CANVAS, '');
+var game = new Phaser.Game(600, 600, Phaser.AUTO, 'game');
 
 game.state.add('play', playState);
 
--- play.js
+++ play.js
@@ -1,23 +1,63 @@
 var playState = {
 
-    board : function() {
-        return new Board();
-    },
-    
     preload: function () {
-        board = new Board();
         board.load();
-        
+
     },
 
     create: function () {
         board.create();
+        this.turns = 0;
     },
 
     update: function () {
-        board.draw();
+        cursors = game.input.keyboard.createCursorKeys();
+
+        if (cursors.up.isDown && board.canMove) {
+            this.turns++;
+            console.log('move up');
+            board.moveUp();
+
+            board.canMove = false;
+            game.time.events.add(300, function () {
+                board.canMove = true;
+            });
+        }
+
+        if (cursors.down.isDown && board.canMove) {
+            this.turns++;
+            console.log('move down');
+            board.moveDown();
+
+            board.canMove = false;
+            game.time.events.add(300, function () {
+                board.canMove = true;
+            });
+        }
+
+        if (cursors.left.isDown && board.canMove) {
+            this.turns++;
+            console.log('move left');
+            board.moveLeft();
+
+            board.canMove = false;
+            game.time.events.add(300, function () {
+                board.canMove = true;
+            });
+        }
+        if (cursors.right.isDown && board.canMove) {
+            this.turns++;
+            console.log('move right');
+            board.moveRight();
+
+            board.canMove = false;
+            game.time.events.add(300, function () {
+                board.canMove = true;
+            });
+        }
+
     },
-    
+
 
 }
 
